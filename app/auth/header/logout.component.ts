05e2cfd1ee4dd6f9cf13dc1287a1e3fc734ba565
Isis Moran <imoran@mail.ccsf.edu>
2018-04-25 14:21:33 -0700

    added searchbar

--- app/auth/header/logout.component.ts
+++ app/auth/header/logout.component.ts
@@ -5,7 +5,8 @@ import { AuthService } from "./auth.service";
 
 @Component({
     selector: 'app-logout',
-    template: `<a (click)="onLogout()">Logout</a>`
+    template: `<a style="color: white; font-size: 18px; font-family: 'Montserrat', sans-serif; cursor:
+    pointer; padding-top: 1px; text-decoration: none;" (click)="onLogout()">Logout</a>`
 })
 export class LogoutComponent {
     constructor(private authService: AuthService, private router: Router) {}
