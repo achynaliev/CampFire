e4d789ff73c57561b6ef168b3346ada03358a5d7
Isis Moran <imoran@mail.ccsf.edu>
2018-04-27 16:56:03 -0700

    test

--- app/userContainer/userContainer.component.ts
+++ app/userContainer/userContainer.component.ts
@@ -19,7 +19,7 @@ export class UserComponent implements OnInit {
     var user = JSON.parse(currentUser);
     console.log(user);
     if(user) {
-      this.imageUrl = "https://avatars0.githubusercontent.com/u/30304723?s=400&u=1d9e86dba7215d12794225083190b3b71875c18d&v=4";
+      this.imageUrl = "http://chittagongit.com//images/generic-user-icon/generic-user-icon-19.jpg";
       this.firstName = user.firstName;
       this.lastName = user.lastName;
       this.username = user.username;
