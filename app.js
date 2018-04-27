6fcef40c38bbb218b2b6a75ebd416a83dbf0b59f
Ujwal Battar <ujwal.battar@gmail.com>
2018-04-27 13:05:30 -0700

    setup

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
+var mLabKeys = require('./configkeys/keys');
 var chatRoutes = require('./routes/chat');
 var appRoutes = require('./routes/app');
 var userRoutes = require('./routes/user');
@@ -14,7 +15,14 @@ var profileRoutes = require('./routes/profile');
 var searchRoutes = require('./routes/search');
 
 var app = express();
-mongoose.connect('mongodb://localhost:27017/firecamp');
+// mongoose.connect('mongodb://localhost:27017/firecamp');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(mLabKeys.mongoURI, function(err) {
+ if (err){
+   console.error("Error!" + err);
+ }
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
