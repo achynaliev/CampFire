07ed80a6c8382246921bdada3a61067f9a269be2
Ujwal Battar <ujwal.battar@gmail.com>
2018-04-27 12:00:10 -0700

    change keys

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
-var mLabKeys = require('./configkeys/keys');
 var chatRoutes = require('./routes/chat');
 var appRoutes = require('./routes/app');
 var userRoutes = require('./routes/user');
@@ -14,15 +13,9 @@ var roomRoutes = require('./routes/room');
 var profileRoutes = require('./routes/profile');
 var searchRoutes = require('./routes/search');
 
-// mongoose.connect('mongodb://localhost:27017/firecamp');
-mongoose.Promise = global.Promise;
-mongoose.connect(mLabKeys.mongoURI, function(err) {
-  if (err){
-    console.error("Error!" + err);
-  }
-});
-
 var app = express();
+mongoose.connect('mongodb://localhost:27017/firecamp');
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
