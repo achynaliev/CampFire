bf66103af88964d5f665f11939d09a834a511594
Isis Moran <imoran@mail.ccsf.edu>
2018-04-29 12:38:32 -0700

    delete ideas

--- app/ideas/idea.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-
-import { Project } from "./projects/project.model";
-import { ProjectService } from "./idea.service";
-
-@Component({
-  selector: 'project-index',
-  templateUrl:
-})
