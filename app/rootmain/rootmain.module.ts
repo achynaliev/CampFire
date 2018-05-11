9f95d544e77ac3a7f415028dcaaff44b99ef4489
Yukichi Kawada <yukichikawada@gmail.com>
2018-05-11 10:27:27 -0700

    Fix naming issue for RootMainRoutingModule, could be refactored to follow convention but working for now

--- app/rootmain/rootmain.module.ts
+++ app/rootmain/rootmain.module.ts
@@ -4,16 +4,17 @@ import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { RootMainComponent } from './rootmain.component';
 import { CurrentRoomComponent } from './currentroom/currentRoom.component';
-import { ProjectsModule } from '../projects/projects.module';
+// import { ProjectsModule } from '../projects/projects.module';
 import { UserComponent } from './userContainer/userContainer.component';
 import { RoomListComponent, RoomComponent } from './rooms';
-import { RootMainRoutes } from './rootmain.routes';
+import { RootMainRoutingModule } from './rootmain.routes';
 // import { ProjectIndexItem, ProjectIndexComponent } from '../projects/project_index';
 
 @NgModule({
   imports: [
     CommonModule,
-    ProjectsModule,
+    // ProjectsModule,
+    RootMainRoutingModule,
     SharedModule
   ],
   declarations: [
